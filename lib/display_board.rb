07c5c3406d0a68f09b09920ebedd19dc9d04e4f5
Daniel Park <park.daniel1@gmail.com>
2020-03-18 18:03:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board
+def display_board(board)
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 end
-display_board
+display_board(board)
