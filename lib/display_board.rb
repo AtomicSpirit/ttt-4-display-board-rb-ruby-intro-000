a1cc6376c74c174ae771816b9b5df2a422e0e046
Daniel Park <park.daniel1@gmail.com>
2020-03-18 18:07:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
 puts "   |   |   "
+puts "X: #{" "[0]}"
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
-puts "X: #{X[0]}"
 end
 display_board(board)
